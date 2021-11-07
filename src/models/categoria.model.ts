204e777a200c10b62b2dea1b78ec0387b5bd988a
Neila Cupitra <necupi@gmail.com>
2021-11-06 21:19:53 -0500

    Versión 1.0.0 de Aplicación Minimercado

---
+++ src/models/categoria.model.ts
@@ -0,0 +1,31 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Producto} from './producto.model';
+
+@model()
+export class Categoria extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+  @hasMany(() => Producto)
+  productos: Producto[];
+
+  constructor(data?: Partial<Categoria>) {
+    super(data);
+  }
+}
+
+export interface CategoriaRelations {
+  // describe navigational properties here
+}
+
+export type CategoriaWithRelations = Categoria & CategoriaRelations;
---
+++ src/models/proveedor.model.ts
@@ -0,0 +1,61 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Producto} from './producto.model';
+
+@model()
+export class Proveedor extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nit: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  razon_social: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  sitio_web: string;
+
+  @hasMany(() => Producto)
+  productos: Producto[];
+
+  constructor(data?: Partial<Proveedor>) {
+    super(data);
+  }
+}
+
+export interface ProveedorRelations {
+  // describe navigational properties here
+}
+
+export type ProveedorWithRelations = Proveedor & ProveedorRelations;
